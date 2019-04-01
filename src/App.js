771e098c0df667be59c2396e34f68473467cc8da
Jhon Arias <jhonarmont@hotmail.com>
2019-04-01 18:14:10 -0500

    Editado el teto del h1 y del p

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Primera aplicación con react</h1>
-        <p>Esta es mi primera aplicacion usando react</p>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
